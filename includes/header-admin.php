05d5c8df0c1378c82cf39af20d79bd62b98dadbe
techotronic <techotronic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-05 22:17:40 +0000

    fixing bug where empty favicon links would be inserted into the page
    git-svn-id: http://plugins.svn.wordpress.org/all-in-one-favicon/trunk@249353 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/header-admin.php
+++ includes/header-admin.php
@@ -9,23 +9,35 @@
  */
 
 function aioFaviconRenderAdminHeader() {
-    $aioFaviconSettings = (array)get_option(AIOFAVICON_SETTINGSNAME);
-    if (! empty($aioFaviconSettings)) {
+    $aioFaviconSettings = (array) get_option(AIOFAVICON_SETTINGSNAME);
+    if (!empty($aioFaviconSettings)) {
         ?><!-- <?php echo AIOFAVICON_NAME ?> <?php echo AIOFAVICON_VERSION ?> | by Arne Franken, http://www.techotronic.de/ --><?php
         foreach ((array) $aioFaviconSettings as $type => $url) {
-            if(preg_match('/backend/i', $type)){
-                if(preg_match('/ico/i', $type)) {
-                    ?><link rel="shortcut icon" href="<?php echo htmlspecialchars($url)?>" /><?php
-                } else if (preg_match('/gif/i', $type)) {
-                    ?><link rel="icon" href="<?php echo htmlspecialchars($url)?>" type="image/gif" /><?php
-                } else if (preg_match('/png/i', $type)) {
-                    ?><link rel="icon" href="<?php echo htmlspecialchars($url)?>" type="image/png" /><?php
-                } else if (preg_match('/apple/i', $type)){
-                    ?><link rel="apple-touch-icon" href="<?php echo htmlspecialchars($url)?>"/><?php
+            if (!empty($url)) {
+                if (preg_match('/backend/i', $type)) {
+                    if (preg_match('/ico/i', $type)) {
+                        ?>
+                        <link rel="shortcut icon" href="<?php echo htmlspecialchars($url)?>"/><?php
+
+                    } else if (preg_match('/gif/i', $type)) {
+                        ?>
+                        <link rel="icon" href="<?php echo htmlspecialchars($url)?>" type="image/gif"/><?php
+
+                    } else if (preg_match('/png/i', $type)) {
+                        ?>
+                        <link rel="icon" href="<?php echo htmlspecialchars($url)?>" type="image/png"/><?php
+
+                    } else if (preg_match('/apple/i', $type)) {
+                        ?>
+                        <link rel="apple-touch-icon" href="<?php echo htmlspecialchars($url)?>"/><?php
+
+                    }
                 }
             }
         }
         ?><!-- <?php echo AIOFAVICON_NAME ?> <?php echo AIOFAVICON_VERSION ?> | by Arne Franken, http://www.techotronic.de/ --><?php
+
     }
 }
+
 ?>
