e786b5c958a75afc0e5e3385fe328cb83586254c
Arne Franken <arne.franken@coremedia.com>
2013-02-17 00:28:48 +0100

    add comments.

--- includes/settings-page/sp-plugin-favicon-settings.php
+++ includes/settings-page/sp-plugin-favicon-settings.php
@@ -25,14 +25,16 @@
                     <div id="<?php echo $iconName ?>-favicon"></div>
                 </td>
                 <td>
+                    <!-- text input field so URLs may be copy'n'pasted -->
                     <input id="<?php echo AIOFAVICON_SETTINGSNAME .'-'. $iconName ?>-text" type="text" name="<?php echo AIOFAVICON_SETTINGSNAME . '[' . $iconName ?>-text]" size="60" maxlength="100000" value="<?php echo $this->aioFaviconSettings[$iconName] ?>"/>
+                    <!-- label for file input, is displayed as upload button. All browsers then trigger file upload but Mozilla, see aiofavicon.js for workaround. -->
                     <label id="<?php echo AIOFAVICON_SETTINGSNAME .'-'. $iconName ?>-button" name="<?php echo $iconName ?>-button" class="button-secondary trigger-file-input" for="<?php echo AIOFAVICON_SETTINGSNAME .'-'. $iconName ?>" ><?php _e('Upload') ?></label>
                     <br />
                     <?php //only display delete checkbox if a favicon was found.
                     if(!empty($this->aioFaviconSettings[$iconName])) { ?>
                     <input type="checkbox" name="delete-<?php echo $iconName ?>"/><?php _e('Check box to delete favicon.',AIOFAVICON_TEXTDOMAIN) ?>
                     <?php } ?>
-                    <!-- input is just not displayed with width:0 and opacity:0 because some browsers will not display the file upload dialog if it's not displayed with display:none -->
+                    <!-- input is hidden with width:0 and opacity:0 because some browsers will not display the file upload dialog if it's hidden with display:none -->
                     <input id="<?php echo AIOFAVICON_SETTINGSNAME .'-'. $iconName ?>" type="file" name="<?php echo $iconName ?>" size="50" maxlength="100000" accept="image/*" value="<?php echo $this->aioFaviconSettings[$iconName] ?>" style="width: 0; opacity: 0;"/>
                 </td>
             </tr>
